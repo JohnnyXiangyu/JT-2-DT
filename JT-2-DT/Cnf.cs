ea41fc70a28b784a86ea02e9d33f7284afcd3efb
Xiangyu Wan <39484269+JohnnyXiangyu@users.noreply.github.com>
2023-04-11 15:40:50 -0700

    added logic for generating moral graph of a CNF

---
+++ JT-2-DT/Cnf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JT_2_DT
+{
+    internal class Cnf
+    {
+        public int VariableCount { get; set; }
+        public IEnumerable<IEnumerable<int>> Clauses { get; set; }
+
+        public Cnf(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JT-2-DT/MoralGraph.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JT_2_DT
+{
+    internal class MoralGraph
+    {
+        public Dictionary<int, HashSet<int>> AllEdgesBySmallVar { get; private set; }
+
+        public MoralGraph(string CnfPath)
+        {
+            Cnf formula = new(CnfPath);
+            AllEdgesBySmallVar = new();
+
+            List<Task> tasks = new List<Task>(formula.VariableCount);
+
+            for (int i = 1; i <= formula.VariableCount; i++)
+            {
+                int variable = i;
+                var newEdges = new HashSet<int>();
+
+                tasks[variable] = Task.Run(() =>
+                {
+                    foreach (IEnumerable<int> clause in formula.Clauses)
+                    {
+                        if (clause.Any(x => x == variable))
+                        {
+                            foreach (int other in clause)
+                            {
+                                if (other > variable)
+                                {
+                                    newEdges.Add(other);
+                                }
+                            }
+                        }
+                        
+                    }
+                });
+
+                AllEdgesBySmallVar[variable] = newEdges;
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        public void OutputToFile(string outPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
