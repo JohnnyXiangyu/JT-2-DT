8c254f89208879f15da8151829cce6075d8fbcde
Xiangyu Wan <wonjohnny@outlook.com>
2023-04-20 16:35:46 -0700

    implemented moral graph serialization

--- JT-2-DT/MoralGraph.cs
+++ JT-2-DT/MoralGraph.cs
@@ -1,30 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JT_2_DT
+﻿namespace JT_2_DT
 {
     internal class MoralGraph
     {
         public Dictionary<int, HashSet<int>> AllEdgesBySmallVar { get; private set; }
+        public int VerticeCount { get => _formula.VariableCount; }
+        public int EdgeCount
+        {
+            get
+            {
+                int edgeCount = 0;
+                foreach ((int _, HashSet<int> set) in AllEdgesBySmallVar)
+                {
+                    edgeCount += set.Count;
+                };
+                return edgeCount;
+            }
+        }
+
+        private readonly Cnf _formula;
 
         public MoralGraph(string CnfPath)
         {
-            Cnf formula = new(CnfPath);
+            _formula = new(CnfPath);
             AllEdgesBySmallVar = new();
 
-            List<Task> tasks = new List<Task>(formula.VariableCount);
+            List<Task> tasks = new List<Task>(_formula.VariableCount);
 
-            for (int i = 1; i <= formula.VariableCount; i++)
+            for (int i = 1; i <= _formula.VariableCount; i++)
             {
                 int variable = i;
                 var newEdges = new HashSet<int>();
 
                 tasks[variable] = Task.Run(() =>
                 {
-                    foreach (IEnumerable<int> clause in formula.Clauses)
+                    foreach (IEnumerable<int> clause in _formula.Clauses)
                     {
                         if (clause.Any(x => x == variable))
                         {
@@ -48,7 +57,17 @@ namespace JT_2_DT
 
         public void OutputToFile(string outPath)
         {
-            throw new NotImplementedException();
+            using var stream = File.OpenWrite(outPath);
+            using var writer = new StreamWriter(stream);
+
+            writer.WriteLine($"p tw {_formula.VariableCount} {EdgeCount}");
+            foreach ((int v1, HashSet<int> neighbours) in AllEdgesBySmallVar)
+            {
+                foreach (int v2 in neighbours)
+                {
+                    writer.WriteLine($"{v1} {v2}");
+                }
+            }
         }
     }
 }
