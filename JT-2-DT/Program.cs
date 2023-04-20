5a6bfb702864fb19160dd333fd56efea50880ec3
Xiangyu Wan <wonjohnny@outlook.com>
2023-04-20 16:19:08 -0700

    prototype now loads a join tree correctly

--- JT-2-DT/Program.cs
+++ JT-2-DT/Program.cs
@@ -1,37 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using JT_2_DT;
+﻿using JT_2_DT;
 
-using Family = System.Collections.Generic.HashSet<int>;
+UndirectedJtree jtree = new(@"Examples\ex001.td");
+Console.WriteLine("tree decomposition loaded");
 
-JoinTree node_1 = new();
-JoinTree node_2 = new();
-JoinTree node_3 = new();
-JoinTree node_4 = new();
 
-// construct the minimal tree
-int a = 1;
-int b = 2;
-int c = 3;
-int d = 4;
-int e = 5;
-
-node_1.Cluster = new(new int[] { a, b, c });
-node_2.Cluster = new(new int[] { b, c, d });
-node_3.Cluster = new(new int[] { c, e });
-node_4.Cluster = new(new int[] { c, e });
-
-JoinTree.Connect(node_2, node_1);
-JoinTree.Connect(node_2, node_3);
-JoinTree.Connect(node_3, node_4);
-
-Family ab = new(new[] { a, b });
-Family ac = new(new[] { a, c });
-Family bcd = new(new[] { b, c, d });
-Family cd = new(new[] { c, e });
-
-node_2.PrintAt();
-
-Console.WriteLine("\nabracadabra\n");
-node_2.MakeDTree(new[] {ab, ac, bcd, cd });
-
-node_2.PrintAt();
--- JT-2-DT/UndirectedJtree.cs
+++ JT-2-DT/UndirectedJtree.cs
@@ -3,22 +3,62 @@
     internal class UndirectedJtree
     {
         private readonly UndirectedGraph _tree;
-        private readonly List<HashSet<int>> _clusterMapping;
+        private readonly List<HashSet<int>> _clusterMapping = new();
         private int _nodeCount;
 
-        public UndirectedJtree(int vertexCount)
+        public UndirectedJtree(string filePath)
         {
-            _nodeCount = vertexCount;
-            _tree = new(vertexCount);
-            _clusterMapping = new List<HashSet<int>>(vertexCount);
-            for (int i = 0; i < vertexCount; i++)
+            var lines = File.ReadAllLines(filePath);
+            if (lines == null)
             {
-                _clusterMapping.Add(new());
+                throw new FileLoadException();
             }
 
-            throw new NotImplementedException();
-            // TODO: add edges one by one
-            // TODO: add clusters one by one
+            foreach (var line in lines)
+            {
+                switch (line[0])
+                {
+                    case 's':
+                        {
+                            var words = line.Split(' ');
+                            int bagCount = int.Parse(words[2]);
+                            // int width = int.Parse(words[3]) - 1;
+
+                            _nodeCount = bagCount;
+                            _tree = new(bagCount);
+                            _clusterMapping = new List<HashSet<int>>(bagCount);
+                            for (int i = 0; i < bagCount; i++)
+                            {
+                                _clusterMapping.Add(new());
+                            }
+
+                            break;
+                        }
+                    case 'b':
+                        {
+                            var words = line.Split(' ');
+                            int clusterIndex = int.Parse(words[1]) - 1;
+
+                            for (int i = 2; i < words.Length; i ++)
+                            {
+                                _clusterMapping[clusterIndex].Add(int.Parse(words[i]));
+                            }
+
+                            break;
+                        }
+                    case 'c':
+                        break;
+                    default:
+                        {
+                            var words = line.Split(' ');
+                            int v1 = int.Parse(words[0]) - 1;
+                            int v2 = int.Parse(words[1]) - 1;
+                            _tree!.AddEdge(v1, v2);
+
+                            break;
+                        }
+                }
+            }
         }
 
         public void MakeDtree(IEnumerable<IEnumerable<int>> families)
