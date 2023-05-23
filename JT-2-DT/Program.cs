8c3936d5ed32b3827dc322787373a742f8def253
Xiangyu Wan <39484269+JohnnyXiangyu@users.noreply.github.com>
2023-05-23 15:21:23 -0700

    adding examples back to main project since it makes more sense to have a working test bench in main too

--- JT-2-DT/Program.cs
+++ JT-2-DT/Program.cs
@@ -2,3 +2,33 @@
 using System.Diagnostics;
 
 Console.WriteLine("hello world");
+
+Stopwatch timer = Stopwatch.StartNew();
+
+Cnf formula = new(Path.Combine("Examples", "sat-grid-pbl-0015.cnf"));
+MoralGraph graph = new(formula);
+
+string tempGrFilename = Path.GetTempFileName();
+graph.OutputToFile(tempGrFilename);
+
+// compute the tree decomposition
+string tempTdFilename = Path.GetTempFileName();
+Process twSolver = new();
+twSolver.StartInfo.FileName = "java";
+twSolver.StartInfo.Arguments = $"-jar {Path.Combine("external_executables", "tw.jar")} {tempGrFilename} {tempTdFilename}";
+twSolver.Start();
+twSolver.WaitForExit();
+
+// compute the dtree
+Dtree dtree = new(tempTdFilename, formula.Clauses);
+foreach (var line in dtree.SerializeAsDtree())
+{
+    Trace.WriteLine(line());
+}
+
+// report time
+Trace.WriteLine($"{timer.Elapsed.TotalSeconds} seconds");
+
+// finally release the temp files
+File.Delete(tempTdFilename);
+File.Delete(tempTdFilename);
