d29cc8eb106d9b7147cfe5e4b499afb2bdc66d24
Johnny Wan <wonjohnny@outlook.com>
2023-05-30 18:47:38 -0700

    implemented tamaki 2017 exact version

--- JT-2-DT/Solvers/Exact/Tamaki2017.cs
+++ JT-2-DT/Solvers/Exact/Tamaki2017.cs
@@ -1,7 +1,27 @@
-﻿namespace JT_2_DT.Solvers.Exact
+﻿using System.Diagnostics;
+
+namespace JT_2_DT.Solvers.Exact
 {
-    internal class Tamaki2017
-    {
-        // TODO: figure out how to configure the command line arguments
-    }
+	internal class Tamaki2017 : ITwSolver
+	{
+		// TODO: figure out how to configure the command line arguments
+		public void Execute(string inputPath, string outputPath)
+		{
+			using Process solver = new();
+			solver.StartInfo.FileName = "java";
+			solver.StartInfo.Arguments = $"-jar {Path.Combine("external_executables", "exact_solvers", "tamaki2017_exact.jar")}";
+			solver.StartInfo.RedirectStandardOutput = true;
+			solver.StartInfo.RedirectStandardInput = true;
+			solver.Start();
+			
+			using (FileStream fs = File.OpenRead(inputPath)) 
+			{
+				fs.CopyTo(solver.StandardInput.BaseStream);
+			}
+			
+			solver.WaitForExit();
+			string result = solver.StandardOutput.ReadToEnd();
+			File.WriteAllText(outputPath, result);
+		}
+	}
 }
